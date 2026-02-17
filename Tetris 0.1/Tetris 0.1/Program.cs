3197c541180e19fe7c66711268e0498317eafb90
John <ninfori384@gmail.com>
2026-02-17 21:36:18 +0300

    создали программу Hello world

---
+++ Tetris 0.1/Tetris 0.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris_0._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
